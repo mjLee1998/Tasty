48b79a0bbbf9bf65ae2b8ed8b5d8541fedecf18e
이명진 <audwls172@gmail.com>
2022-03-29 00:05:14 +0900

    3.29-1
    js의 for문에 php를 넣어보자

--- test.php
+++ test.php
@@ -1,3 +1,34 @@
+				<?php
+				
+				session_start();
+				include"./inc/dbcon.php";
+				
+				$sql = "select * from restaurants;";
+				
+				$result = mysqli_query($dbcon,$sql);
+				$row = mysqli_num_rows($result);
+				echo $row;
+				echo "<br>";
+				
+				
+				$set = "set @rownum = 0;";
+				mysqli_query($dbcon,$set);
+				$result = mysqli_query($dbcon,$sql);
+				echo "<br>";
+				
+				
+				for($i = 1; $i <= $row; $i=$i+1){
+					$sql = "select R.* from (select @rownum:=@rownum+1 as row, A.* from restaurants A where (@rownum:=0)=0) R where row = $i;";
+				
+					$result = mysqli_query($dbcon,$sql);
+					$rows = mysqli_fetch_assoc($result);
+					echo "<br>";
+					echo $i;
+					echo "<br>";
+					echo "<br>";
+				}
+				
+					?>
 
 
 <!DOCTYPE html>
@@ -11,76 +42,18 @@
 	<body>
 		<script>
 			var positions = [];
-			</script>
-</body>
+			var i;
+			for(i = 1; i <= <?php echo $row ?>; i++){
+				<?php
+					$sql = "select R.* from (select @rownum:=@rownum+1 as row, A.* from restaurants A where (@rownum:=0)=0) R where row = $i;";
+				
+					$result = mysqli_query($dbcon,$sql);
+					$rows = mysqli_fetch_assoc($result);
+					echo "console.log($i)";
+				?>
+			}
+
+		</script>
+	</body>
 
 </html>
-<?php
-
-session_start();
-include"./inc/dbcon.php";
-
-$sql = "select * from restaurants;";
-
-$result = mysqli_query($dbcon,$sql);
-$row = mysqli_num_rows($result);
-echo $row;
-echo "<br>";
-
-
-$set = "set @rownum = 0;";
-mysqli_query($dbcon,$set);
-$result = mysqli_query($dbcon,$sql);
-echo "<br>";
-for($i = 1; $i <= $row; $i=$i+1){
-	$sql = "select R.* from (select @rownum:=@rownum+1 as row, A.* from restaurants A where (@rownum:=0)=0) R where row = $i;";
-	// echo $sql;
-	$result = mysqli_query($dbcon,$sql);
-	$rows = mysqli_fetch_assoc($result);
-	echo "<br>";
-	echo $i;
-
-	echo "<br>";
-	echo "{index: 1, title: '올래곱창', latlng: new kakao.maps.LatLng($row['location'] ?>), content: '<div class=".
-		"overlaybox".
-		">' + '<div class=".
-		"boxtitle".
-		">$row[".
-		"restaurantName".
-		"]?></div>' +'    <div class=".
-		"first".
-		">' + '<div class=".
-		"triangle".
-		"></div>' +'<div class=".
-		"categori".
-		">$row[".
-		"categori".
-		"]?></div>' +'    </div>' +'    <div class=".
-		"instaId".
-		">$row[".
-		"instaId".
-		"]?></div>' +'    <ul class=".
-		"information".
-		">' +'        <li class=".
-		"address".
-		">' +'            <div class=".
-		"addr".
-		">주소</div>' +'            <div class=".
-		"addr1".
-		">$row[".
-		"addr1".
-		"]?></div>' +'            <div class=".
-		"addr2".
-		">$row[".
-		"addr2".
-		"]?></div>' +'        </li>' +'        <li class=".
-		"review".
-		">' +'            <div class=".
-		"review1".
-		">한줄평</div>' +'            <div class=".
-		"review2".
-		">$row[".
-		"review".
-		"]?></div>' +'        </li>' +'    </ul>' +'</div>'}";
-}
-	?>
