a44454cd86c98024f35eda33034f92d94ff19c73
이명진 <audwls172@gmail.com>
2022-04-10 23:08:01 +0900

    4.10-2
    dbcon에 db암호 넣기

--- inc/dbcon.php
+++ inc/dbcon.php
@@ -1,4 +1,5 @@
 <?php
-($dbcon = mysqli_connect('223.26.138.80', 'user', '', 'tasty')) or die('접속에 실패했습니다.');
+($dbcon = mysqli_connect('localhost', 'root', 'qlalfqjsgh159!', 'tasty')) or
+    die('접속에 실패했습니다.');
 mysqli_set_charset($dbcon, 'utf8');
 ?>
