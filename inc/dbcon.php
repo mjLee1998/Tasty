832c893f5752ef76eed68e589a295de85f6708bf
이명진 <audwls172@gmail.com>
2022-04-11 14:27:16 +0900

    4.11-1
    계속된 dbcon오류
    
    일단 index의 dbcon에서 오류가 발생해 홈페이지에 접속 불가
    
    정확히는 외부에서 내 db에 접속하지 못하는듯 하다.

--- inc/dbcon.php
+++ inc/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-($dbcon = mysqli_connect('localhost', 'root', 'qlalfqjsgh159!', 'tasty')) or
+($dbcon = mysqli_connect('192.168.35.111', 'root', 'qlalfqjsgh159!', 'tasty')) or
     die('접속에 실패했습니다.');
 mysqli_set_charset($dbcon, 'utf8');
 ?>
